fc0fb9ce3c2f83d77b22cb5b14d005f8da237eb8
sonmezonur <onursonmez6116@gmail.com>
2017-01-27 00:25:21 +0200

    Update BirdMotion.sv

--- FLAB_Project/BirdMotion.sv
+++ FLAB_Project/BirdMotion.sv
@@ -25,9 +25,9 @@ module BirdMotion(input logic up,cont,clk,reset,
                   output logic[23:0] head,
                   output logic [23:0]tail,
                   output logic gameOver );
-typedef enum logic[4:0]{S0,S1,S2,S3,S4,S5,S6,S7,S8,
-                        S9,S10,S11,S12,S13,S14,S15,
-                        S16,S17,S18,S19,S20,S21,S22,S23,S24,S25} stateType;
+typedef enum logic[4:0]{S0,S1,S2,S3,S4,S5,S6,S7,S8,S9,
+                        S10,S11,S12,S13,S14,S15,S16,S17,
+                        S18,S19,S20,S21,S22,S23,S24,S25} stateType;
                         
 stateType[4:0] state,nextState;
 
